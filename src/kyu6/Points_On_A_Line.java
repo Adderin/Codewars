537bc0b0598bf1e249124283025682386ca50032
Adderin <adderin@gmail.com>
2019-12-09 21:59:39 +0100

    creating others

--- src/kyu6/Points_On_A_Line.java
+++ src/kyu6/Points_On_A_Line.java
@@ -3,7 +3,7 @@ package kyu6;
 public class Points_On_A_Line {
 
     public static void main(String[] args) {
-        Points_On_A_Line.middle_point(1,2,3, 4,5,6, 7,8,9);
+        System.out.println(Points_On_A_Line.middle_point(-1,0,2, -2,4,-1, -3,8,-4));
     }
 
 
@@ -15,14 +15,14 @@ public class Points_On_A_Line {
         int b = (int) Distance(x1,y1,z1,x3,y3,z3);
         int c = (int) Distance(x2,y2,z2,x3,y3,z3);
         if(a > b && a > c){ return 3;}
-        else if (b > a && b > c){ return 2;}
+        else if (b >= a && b >= c){ return 2;}
         else {return 1;}
 
     }
     public static double Distance(double a1, double b1, double c1,
                                double a2, double b2, double c2)
     {
-        double distance = Math.sqrt(Math.abs((Math.pow((a2-a1),2) + Math.pow((b2-b1),2) + Math.pow((c2-c1),2))));
-        return distance;
+        double distance;
+        return distance= Math.sqrt(Math.abs((Math.pow((a2-a1),2) + Math.pow((b2-b1),2) + Math.pow((c2-c1),2))));
     }
 }
