bd79cf3a95a0609bd980791caa1f743d6e049e2b
Adderin <adderin@gmail.com>
2019-12-11 01:00:31 +0100

    meeting not finished

--- src/kyu6/Meeting.java
+++ src/kyu6/Meeting.java
@@ -1,31 +1,37 @@
 package kyu6;
 
-import javax.imageio.stream.ImageInputStream;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 class Meeting {
-   public static String s = "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill";
+   public static String s = "Alex:STAN;Michael:Wahl;Anna:Gates;Ann:Arno;Robert:Dorries;Ann:Meta;Ann:Gates;Ann:Meta;Alex:STAN;Alex:Schwarz;Michael:Bell;Grace:Bell;Amandy:Wahl;Paul:Wahl;Ann:Bell;Anna:Bell;Megan:Arno;Alex:Kern;Sarah:Bell;Alex:Kor";
 
 
     public static String meeting(String s) {
-
+        s = s.toUpperCase();
         String[] list = s.split(";");
+        System.out.println(Arrays.toString(list));
+        System.out.println(list.length);
         String[] result = new String[list.length];
 
-                for(int i=0; i <  list.length-1; i++) {
+                for(int i=0; i < result.length; i++) {
+                    System.out.println(list[i]);
                     String[] tempMember = list[i].split(":");
                     String temp = tempMember[1];
                     tempMember[1] = tempMember[0];
                     tempMember[0] = temp;
-                    for(int j=0; j < tempMember.length-1; j++) {
-                       result[i] = Arrays.toString(tempMember);
-                       }
-                    }
-        Arrays.sort(result);
-        System.out.println(Arrays.toString(result).toUpperCase());
+                    //System.out.println(temp);
+                    result[i] = Arrays.toString(tempMember);
+                   }
+                Arrays.sort(result);
+                String st = new String();
+                for(String str: result) {
+                    st = st + str;
+                    System.out.println(st);
+                }
+
+        st = st.replace('[','(');
+        st = st.replace(']',')');
+        System.out.println(st);
 
         return s;
     }
@@ -35,5 +41,3 @@ class Meeting {
 
     }
 }
-//SAMPLE OUTPUT
-//"(CORWILL, ALFRED)(CORWILL, FRED)(CORWILL, RAPHAEL)(CORWILL, WILFRED)(TORNBULL, BARNEY)(TORNBULL, BETTY)(TORNBULL, BJON)"
