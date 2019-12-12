346658516e9e7ce0b4c54cbd6d1cfead42d4838d
Adderin <adderin@gmail.com>
2019-12-12 23:42:36 +0100

    String practice

--- src/others/StringPracticing.java
+++ src/others/StringPracticing.java
@@ -5,10 +5,19 @@ import java.util.Arrays;
 public class StringPracticing {
 
 public static void main(String[] args) {
-        String[] list = {"[BELL, ANN]", "[BELL, ANNA]", "BELL, ANNA", "BELL, ANN"};
-        Arrays.sort(list);
-        for(String st : list)
-            System.out.println(st);
+//        String[] list = {"[BELL, ANN]", "[BELL, ANNA]", "BELL, ANNA", "BELL, ANN"};
+//        Arrays.sort(list);
+//        for(String st : list)
+//            System.out.println(st);
+    StringBuilder sb = new StringBuilder();
+    sb.append("Hello there");
+    sb.append("! Everything's good?");
+    System.out.println(sb);
+
+    String st = new String();
+    System.out.println(st.concat("Thank you for calling us"));
+    System.out.println(st+"hmmm");
+    System.out.println(st.concat("! We are so happy"));
     }
 }
 
