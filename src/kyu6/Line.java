0eb10d787b4c4d51e836983a7887c55b7918247c
Adderin <adderin@gmail.com>
2019-12-14 21:18:25 +0100

    Line second iteration

--- src/kyu6/Line.java
+++ src/kyu6/Line.java
@@ -1,15 +1,28 @@
 package kyu6;
 
+import java.util.LinkedList;
+
 public class Line {
+    int[] customers = {25, 25, 50};
+    LinkedList<Integer> cashbox = new LinkedList<Integer>();
+
     public static void main(String[] args) {
 
     }
 
-    public static void TicketSale(){
+    public void TicketSale(int[] customers) {
+        for (int i = 0; i < customers.length; i++) {
+            if(customers[i] == 25) {
+                cashbox.add(customers[i]);
+            }
+            else if(customers[i] == 50 || customers[i] == 100) {
 
+            }
+
+        }
     }
+        public static void ChangeOperations(int[] ticket){
 
-    public static void ChangingCheque(int ticket){
 
     }
 }
