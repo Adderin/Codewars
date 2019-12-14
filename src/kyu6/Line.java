0f22b6a6f8e0ed6e1613262a37e6074291bce58c
Adderin <adderin@gmail.com>
2019-12-14 20:10:55 +0100

    Line first implementation

---
+++ src/kyu6/Line.java
@@ -0,0 +1,15 @@
+package kyu6;
+
+public class Line {
+    public static void main(String[] args) {
+
+    }
+
+    public static void TicketSale(){
+
+    }
+
+    public static void ChangingCheque(int ticket){
+
+    }
+}
