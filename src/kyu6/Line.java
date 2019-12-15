20fec9d952382072cc0eff5ced57516147f1c189
Adderin <adderin@gmail.com>
2019-12-15 19:06:37 +0100

    Line finished

--- src/kyu6/Line.java
+++ src/kyu6/Line.java
@@ -1,17 +1,19 @@
 package kyu6;
 
 public class Line {
-    static int[] customers = {25, 25, 50, 25, 50, 100};
+    static int[] customers = {25, 25, 25, 25, 50, 100, 50};
     public static int count_25 = 0;
     public static int count_50 = 0;
     public static int check = 0;
 
     public static void main(String[] args) {
+
         System.out.println("Sale was successful: " + TicketSale(customers));
         System.out.printf("25: %d and 50: %d", count_25, count_50);
     }
 
     public static String TicketSale(int[] customers) {
+
         for (int i = 0; i < customers.length; i++) {
             switch (customers[i]) {
                 case 25:
@@ -26,7 +28,6 @@ public class Line {
                     check = ChangeOperations(customers[i]);
                     if(check==1) return "NO";
                     break;
-
             }
         }
     return "YES";}
@@ -36,7 +37,8 @@ public class Line {
             case 50:
                 if (count_25 >= 1) {
                     count_25--;
-                } else return 1;
+                }
+                else return 1;
                 break;
             case 100:
                 if (count_50 >= 1 && count_25 >= 1) {
