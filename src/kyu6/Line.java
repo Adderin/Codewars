df858c39184ef533801888fc137f012c59735299
Adderin <adderin@gmail.com>
2019-12-15 17:47:39 +0100

    Line third iteration

--- src/kyu6/Line.java
+++ src/kyu6/Line.java
@@ -1,28 +1,53 @@
 package kyu6;
 
-import java.util.LinkedList;
-
 public class Line {
-    int[] customers = {25, 25, 50};
-    LinkedList<Integer> cashbox = new LinkedList<Integer>();
+    static int[] customers = {25, 25, 50};
+    public static int count_25 = 0;
+    public static int count_50 = 0;
+    public static int count_100 = 0;
 
     public static void main(String[] args) {
-
+        TicketSale(customers);
+        System.out.printf("25: %d and 50: %d", count_25, count_50);
     }
 
-    public void TicketSale(int[] customers) {
+    public static String TicketSale(int[] customers) {
         for (int i = 0; i < customers.length; i++) {
-            if(customers[i] == 25) {
-                cashbox.add(customers[i]);
+            switch (customers[i]) {
+                case 25:
+                    count_25++;
+                    break;
+                case 50:
+                    ChangeOperations(customers[i]);
+                    count_50++;
+                    break;
+                case 100:
+                    ChangeOperations(customers[i]);
+                    count_100++;
+                    break;
+                default:
+                    System.out.println("Something went wrong!");
+                    return "NO";
             }
-            else if(customers[i] == 50 || customers[i] == 100) {
+        }
+    return "YES";}
 
-            }
-
+    public static void ChangeOperations(int ticket) {
+        switch (ticket) {
+            case 50:
+                if (count_25 >= 1) {
+                    count_25--;
+                }
+                break;
+            case 100:
+                if (count_50 >= 1 && count_25 >= 1) {
+                    count_50--;
+                    count_25--;
+                } else if (count_25 >= 3) {
+                    count_25 = count_25 - 3;
+                }
+                break;
         }
     }
-        public static void ChangeOperations(int[] ticket){
 
-
-    }
 }
---
+++ src/others/ArrayListPractice.java
@@ -0,0 +1,22 @@
+package others;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class ArrayListPractice {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        for (int i=0; i<5;i++) {
+            list.add(scanner.nextInt());
+            System.out.println(list.get(i));
+        }
+        Collections.sort(list);
+        list.remove(0);
+        for(int y: list){
+            System.out.print(y+" ");
+
+        }
+    }
+}
