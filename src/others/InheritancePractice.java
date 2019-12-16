e28fa97e882d381c70f063480cb6534b7d670515
Adderin <adderin@gmail.com>
2019-12-16 11:24:00 +0100

    OOP Practice

---
+++ src/others/InheritancePractice.java
@@ -0,0 +1,28 @@
+package others;
+
+class InheritancePractice {
+    private int data;
+
+    public InheritancePractice(){
+        data = 5;
+    }
+
+    public int getData() {
+        return this.data;
+    }
+}
+class Heir extends InheritancePractice{
+    private int data;
+    public Heir(){
+        data = 6;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public static void main(String[] args) {
+        Heir myData = new Heir();
+        System.out.println(myData.getData());
+    }
+}
