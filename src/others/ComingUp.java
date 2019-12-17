236fa1547130dedd0e4097e633fdfc3eee24d1c4
Adderin <adderin@gmail.com>
2019-12-17 12:30:37 +0100

    practicing coming up for values

---
+++ src/others/ComingUp.java
@@ -0,0 +1,16 @@
+package others;
+
+class ComeUp
+        {
+    int data;
+    ComeUp() {
+         data = 10;
+              }
+}
+
+public class ComingUp {
+    public static void main(String[] args) {
+        ComeUp come = new ComeUp();
+        System.out.println(come.data);
+    }
+}
